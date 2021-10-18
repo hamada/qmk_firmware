e03bc4069044e62178c612fdc03317f0a9f0ec3d
hamada <hamada@hey.com>
2021-10-18 18:27:25 +0900

    tidy up lower layer

--- keyboards/ginkgo/keymaps/default/keymap.c
+++ keyboards/ginkgo/keymaps/default/keymap.c
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  DF(_BASE), KC_LALT, KC_LGUI, KC_SPC, MO(_LOWER),       MO(_LOWER), KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
     ),
     [_LOWER] = LAYOUT(
-      KC_GRV, KC_8, DF(_BASE), MO(_LOWER), KC_MINS, KC_EQL,  KC_BSPC,       KC_7, KC_8, KC_9,    KC_0, KC_MINS, KC_EQL,  KC_BSPC,
-      KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_BSLS,                       KC_Y, KC_U, KC_I, KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_H, KC_J, KC_K, KC_L, KC_QUOT,  KC_ENT,                             KC_H, KC_J, KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
-      KC_Y, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,                                KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-      KC_LEFT,  DF(_BASE), KC_DOWN, KC_UP, KC_RGHT, MO(_LOWER),       MO(_LOWER), KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+      KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                            KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_ESC,  DF(_BASE), KC_TRNS, KC_TRNS, KC_TRNS, MO(_LOWER),       MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
