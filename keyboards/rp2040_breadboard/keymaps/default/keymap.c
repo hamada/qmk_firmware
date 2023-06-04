ea2e30f0396a139d51894d7fcb1375ccba8d1a34
hamada <a@hama.day>
2023-06-04 18:01:26 +0900

    fix

--- keyboards/rp2040_breadboard/keymaps/default/keymap.c
+++ keyboards/rp2040_breadboard/keymaps/default/keymap.c
@@ -4,8 +4,15 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_2x4(
-        KC_P1,   KC_P2,   KC_P3,   KC_P4,
-        KC_P5,   KC_P6,   KC_P7,   KC_P8
+    /* [0] = LAYOUT_ortho_1x4( */
+        /* KC_P1,   KC_P2,   KC_P3,   KC_P4 */
+    /* ) */
+    /* [0] = LAYOUT_ortho_2x4( */
+        /* KC_P1,   KC_P2,   KC_P3,   KC_P4, */
+        /* KC_P5,   KC_P6,   KC_P7,   KC_P8 */
+    /* ) */
+    [0] = LAYOUT_ortho_2x8(
+        KC_P1,   KC_P2,   KC_P3,   KC_P4, KC_P5, KC_P6, KC_P7, KC_P8,
+        KC_P9,   KC_P0,   KC_A,    KC_B,  KC_C,  KC_D,  KC_E,  KC_F
     )
 };
