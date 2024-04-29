3c1db19d7e2faf1db2de0b22967339f917585fad
hamada <a@hama.day>
2024-04-29 09:02:58 +0900

    add key layout and default mappings for foliage67

--- keyboards/rp2040_foliage67/config.h
+++ keyboards/rp2040_foliage67/config.h
@@ -3,9 +3,12 @@
 
 #pragma once
 
+
+/* This seems default. But set explicitly. */
+/* firmware without setting theese explicitly doesn't work as split keyboard. */
+#define MASTER_RIGHT
+
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 #define DEBOUNCE 5
-
-// #define MASTER_RIGHT
